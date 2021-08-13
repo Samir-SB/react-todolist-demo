b4a2fabe9d4f909bcd1fed6a039e03427aa1be22
samir-sb <samir.belhadi@univ-batna2.dz>
2021-08-13 15:18:48 +0100

    use ES6 feature.

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,9 @@
 export default function Item({item, deleteItem}) {
+  const {id, title} = item;
   return (
     <div className='d-flex center'>
-      <p>{item.id} - {item.title}</p>
-      <button className='btn btn-danger' onClick={() => deleteItem(item.id)}>delete</button>
+      <p>{id} - {title}</p>
+      <button className='btn btn-danger' onClick={() => deleteItem(id)}>delete</button>
     </div>
   )
 }
