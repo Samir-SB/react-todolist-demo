878bebc9d430d94900d9b07b434d4306f602af2e
samir-sb <samir.belhadi@univ-batna2.dz>
2021-08-13 17:48:37 +0100

    add edit button

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
+
 export default function Item({ item, deleteItem, updateItem }) {
+  const [editable, setEditable] = useState(false);
   const { id, title, completed } = item;
 
   return (
     <div className='d-flex center'>
-      <p onClick={() => updateItem({ ...item, completed: !completed })} className='pointer'>
-        {id} - <span className={completed ? 'completed' : ''}>{title}</span>
-      </p>
+      {editable ? (
+        <input type='text' value={title} onChange={(e) => updateItem({ ...item, title: e.currentTarget.value })} />
+      ) : (
+        <p onClick={() => updateItem({ ...item, completed: !completed })} className='pointer'>
+          {id} - <span className={completed ? 'completed' : ''}>{title}</span>
+        </p>
+      )}
+      <button className={`btn btn-${editable ? 'success' : 'primary'}`} onClick={() => setEditable(!editable)}>
+        {editable ? 'Save' : 'Edit'}
+      </button>
       <button className='btn btn-danger' onClick={() => deleteItem(id)}>
         delete
       </button>
