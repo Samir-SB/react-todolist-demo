704e71e79b15edc23aa3589725b6134a86616613
samir-sb <samir.belhadi@univ-batna2.dz>
2021-08-15 18:05:30 +0100

    refactor item tags and replace p tag with checkbox tag

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -7,11 +7,17 @@ export default function Item({ item, deleteItem, updateItem }) {
   return (
     <div className='d-flex center'>
       {editable ? (
-        <input type='text' value={title} onChange={(e) => updateItem({ ...item, title: e.currentTarget.value })} />
+        <input type='text' value={title} onChange={(e) => updateItem({ ...item, title: e.target.value })} />
       ) : (
-        <p onClick={() => updateItem({ ...item, completed: !completed })} className='pointer'>
-          {id} - <span className={completed ? 'completed' : ''}>{title}</span>
-        </p>
+        <label className={`pointer ${completed ? 'completed' : ''}`}>
+          <input
+            type='checkbox'
+            defaultChecked={completed}
+            hidden={true}
+            onChange={() => updateItem({ ...item, completed: !completed })}
+          />
+          {id} - {title}
+        </label>
       )}
       <button className={`btn btn-${editable ? 'success' : 'primary'}`} onClick={() => setEditable(!editable)}>
         {editable ? 'Save' : 'Edit'}
